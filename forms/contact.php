b6f4f94db9508da695869d9802a77a9225b4d5ed
Akshay RS <66065737+Akshay-Unni@users.noreply.github.com>
2025-05-31 15:57:34 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,24 +1,45 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $to = "wwaypointtech@gmail.com";  // Replace with your email
-    $subject = "New Contact Form Submission";
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
 
-    $name = htmlspecialchars($_POST["name"]);
-    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-    $message = htmlspecialchars($_POST["message"]);
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'wwaypointtech@gmail.com';
 
-    $headers = "From: $email" . "\r\n" .
-               "Reply-To: $email" . "\r\n" .
-               "Content-type: text/plain; charset=UTF-8";
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
 
-    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
 
-    if (mail($to, $subject, $body, $headers)) {
-        echo "Thank you! Your message has been sent.";
-    } else {
-        echo "Sorry, there was an error sending your message.";
-    }
-} else {
-    echo "Invalid request.";
-}
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  
+  $contact->smtp = array(
+    'host' => 'smtp.gmail.com',
+    'username' => 'wwaypointtech@gmail.com',
+    'password' => 'cwav cnkd yeiw velx',
+    'port' => '587'
+  );
+  
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  if(isset($_POST['phone'])) {
+    $contact->add_message( $_POST['phone'], 'Phone');
+  }
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
 ?>
+
